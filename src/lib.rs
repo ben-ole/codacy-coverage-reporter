73fe32e9d186503cbe9860cac80b782ae2eb97f4
Benjamin Müller <benjamin@7mind.de>
2017-12-21 20:53:37 +0100

    set content type to application/json and round coverage numbers

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
+extern crate clap;
+extern crate reqwest;
+
 #[macro_use]
 extern crate serde_json;
 
 #[macro_use]
 extern crate serde_derive;
 
-extern crate clap;
-extern crate reqwest;
-
-#[macro_use] 
+#[macro_use]
 extern crate hyper;
 
+use hyper::header::{Headers, ContentType};
 use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
@@ -17,7 +18,7 @@ use std::error::Error;
 #[derive(Debug, Serialize, Deserialize)]
 struct FileReport {
     filename: String,
-    total: f64,
+    total: u8,
 }
 
 header! { (ProjectToken, "project_token") => [String] }
@@ -26,26 +27,26 @@ pub fn run(config: clap::ArgMatches) -> Result<(), Box<Error>> {
 
   let source = read_source( config.value_of("INPUT").unwrap() )?;
 
-  let cov = source["coverage"].clone();
+  let cov = source["coverage"].as_f64().unwrap() * 100.0;
 
   let target = select_target(&source)?;
 
   let body = json!({
-    "total": cov,
+    "total": cov.round() as u8,
     "fileReports": file_reports(&target, config.value_of("PREFIX").unwrap_or(""))
   });
 
   if let Some(ref path) = config.value_of("OUTPUT") {
     let mut file = File::create(path)?;
-    file.write_all(body.to_string().as_bytes())?;  
+    file.write_all(body.to_string().as_bytes())?;
   }
-  
+
   if config.occurrences_of("v") == 2 {
     println!("{}", body);
   }
 
-  send( body.to_string(), 
-        config.value_of("COMMIT").unwrap(), 
+  send( body.to_string(),
+        config.value_of("COMMIT").unwrap(),
         config.value_of("LANGUAGE").unwrap_or("swift"),
         config.value_of("PROJECT_TOKEN").unwrap() )
 }
@@ -63,7 +64,7 @@ fn read_source(path: &str) -> Result<serde_json::Value, Box<Error>> {
   Ok( serde_json::from_str(&contents)? )
 }
 
-fn file_reports(source: &serde_json::Value, prefix: &str) -> serde_json::Value {  
+fn file_reports(source: &serde_json::Value, prefix: &str) -> serde_json::Value {
 
   let files = &source["files"].as_array().unwrap();
 
@@ -71,8 +72,8 @@ fn file_reports(source: &serde_json::Value, prefix: &str) -> serde_json::Value {
     let name = f["name"].as_str().unwrap();
     let filename = prefix.to_owned() + name;
 
-    FileReport {  filename, 
-                  total: f["coverage"].as_f64().unwrap() * 100.0 }
+    let coverage = f["coverage"].as_f64().unwrap() * 100.0;
+    FileReport { filename, total: coverage.round() as u8 }
 
   }).collect();
 
@@ -87,18 +88,22 @@ fn select_target(source: &serde_json::Value) -> Result<&serde_json::Value, Box<E
 
 fn send(body: String, commit_uuid: &str, language: &str, project_token: &str) -> Result<(),Box<Error>> {
   let client = reqwest::Client::new();
-  
+
   let path = "https://api.codacy.com/2.0/coverage/".to_owned() + commit_uuid + "/" + language;
   let url = reqwest::Url::parse(&path)?;
 
-  let bytes: Vec<u8> = body.into_bytes();
+  let bytes = body.into_bytes();
+
+  let mut headers = Headers::new();
+  headers.set(ContentType::json());
+  headers.set(ProjectToken(project_token.to_string()));
 
   let req = client.post(url)
     .body(bytes)
-    .header(ProjectToken(project_token.to_string()))    
+    .headers(headers)
     .build()?;
 
-  println!("request = {:?}", req);    
+  println!("request = {:?}", req);
 
   let mut resp = client.execute(req)?;
 
